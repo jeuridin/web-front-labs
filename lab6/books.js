89414377cdc8fb4800f089515dc274767b078353
Janna Azaryan <azaryan05@icloud.com>
2025-05-11 15:21:19 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,88 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: '350 ₽',
+        bestseller: true,
+        available: false,
+    },
+
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: '250 ₽',
+        bestseller: false,
+        available: true,
+    },
+
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: '450 ₽',
+        bestseller: true,
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: 1984,
+        publicationYear: 1984,
+        price: '200 ₽',
+        bestseller: false,
+        available: true,
+    },
+
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1966,
+        price: '258 ₽',
+        bestseller: false,
+        available: true,
+    },
+
+    {
+        author: 'Харпер Ли',
+        name: 'Убить пересмешника',
+        publicationYear: 1960,
+        price: '452 ₽',
+        bestseller: false,
+    },
+
+    {
+        author: 'Федор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: ' 189 ₽',
+        bestseller: false,
+        available: false,
+    },
+
+    {
+        author: 'Джейн Остин',
+        name: 'Гордость и предубеждение',
+        publicationYear: 1813,
+        price: '279 ₽',
+        bestseller: true,
+        available: true,
+    },
+
+    {
+        author: 'Габриэль Гарсия Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 1967,
+        price: '678 ₽',
+        bestseller: false,
+        available: false,
+    },
+
+    {
+        author: 'Фрэнсис Скотт Фицджеральд',
+        name: 'Великий Гэтсби',
+        publicationYear: 1925,
+        price: '458 ₽',
+        bestseller: true,
+        available: true,
+    },
+];
